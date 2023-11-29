98d2c992aeedcdf64ea3ef7b9b5d1c264a6bff3c
sjy2001 <107793192+sjy2001@users.noreply.github.com>
2023-11-30 05:35:06 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from collections import Counter
 # Initialize variables to store the read data
 problem_name = ""         # Store the name of the problem
 knapsack_data_type = ""   # Store the data type of the knapsack problem
@@ -8,14 +9,14 @@ min_speed = 0.0           # Store the minimum speed
 max_speed = 0.0           # Store the maximum speed
 renting_ratio = 0.0       # Store the renting ratio
 edge_weight_type = ""      # Store the type of edge weight
-node_coord_section = []    # Store the list of node coordinate information
+node_coord_section = []    # Store the list of node coordinates
 items_section = []         # Store the list of item information
 
 # Open the file
-with open('a280-n279.txt', 'r') as file:
+with open('a280-n1395.txt', 'r') as file:
     # Read the file content line by line
     for line in file:
-        # Remove trailing newline characters
+        # Remove the newline character at the end of the line
         line = line.strip()
 
         # Parse the file content line by line
@@ -41,8 +42,8 @@ with open('a280-n279.txt', 'r') as file:
             # Start reading node coordinate data
             break
         elif line:  # If the line is not empty
-            # Process other possible information
-            print("Other Information:", line)
+            # Handle other possible information
+            print("Other information:", line)
 
     # Continue reading node coordinate data
     for line in file:
@@ -63,7 +64,39 @@ with open('a280-n279.txt', 'r') as file:
             index, profit, weight, assigned_node_number = map(int, line.split())
             items_section.append((index, profit, weight, assigned_node_number))
 
-# Print the read information
+# Convert items_section into an array and group by "ASSIGNED NODE NUMBER"
+items_array = []
+
+# Use a dictionary to group rows with the same "ASSIGNED NODE NUMBER"
+grouped_items = {}
+for item in items_section:
+    assigned_node_number = item[3]  # Index of "ASSIGNED NODE NUMBER" is 3
+
+    # If the current ASSIGNED NODE NUMBER already exists in the dictionary, add to the group
+    if assigned_node_number in grouped_items:
+        grouped_items[assigned_node_number].append(item)
+    # Otherwise, create a new group
+    else:
+        grouped_items[assigned_node_number] = [item]
+
+# Add grouped data to the array
+for group in grouped_items.values():
+    items_array.append(group)
+
+
+# Convert grouped data to an array and record the count of ASSIGNED NODE NUMBER
+items_array = []
+assigned_node_counts = Counter()
+
+for group in grouped_items.values():
+    items_array.append(group)
+
+    assigned_node_numbers = [item[3] for item in group]  # Get all ASSIGNED NODE NUMBER in the current group
+    node_number_counts = Counter(assigned_node_numbers)  # Count the occurrences of ASSIGNED NODE NUMBER
+
+    # Update the total counter with the count of ASSIGNED NODE NUMBER
+    assigned_node_counts.update(node_number_counts)
+# Print the results
 print("PROBLEM NAME:", problem_name)
 print("KNAPSACK DATA TYPE:", knapsack_data_type)
 print("DIMENSION:", dimension)
@@ -74,4 +107,5 @@ print("MAX SPEED:", max_speed)
 print("RENTING RATIO:", renting_ratio)
 print("EDGE_WEIGHT_TYPE:", edge_weight_type)
 print("NODE_COORD_SECTION:", node_coord_section)
-print("ITEMS SECTION:", items_section)
+print("ITEMS SECTION:", items_array)
+print(f"{node_number_counts}")
